5b32a4a47c222818513a973c11088c99dc102e4d
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-30 16:53:41 +0100

    Create App style

---
+++ src/app.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Wrapper = styled.main`
+  background-color: #021E36;
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-size: 3rem;
+  }
+`;
