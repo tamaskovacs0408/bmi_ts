256174a0a99333050a0181b613cad705a7393ccd
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-30 17:10:19 +0100

    Import Calculator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Wrapper} from './app.style';
-import Cal
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <Wrapper>
       <h1>BMI Calculator</h1>
-      
+      <Calculator />
     </Wrapper>
   );
 }
