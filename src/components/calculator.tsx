ce526848d6b1266cf47696feb05e5cdd37558ddf
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-30 17:17:51 +0100

    Create form for the Calculator

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import {Wrapper} from './calculator.style';
 
-const Calculator = () => {
+const Calculator: React.FC = () => {
   return (
     <Wrapper>
-      
+      <form>
+        <input
+          type="number"
+          step="0.1"
+          placeholder="Enter you weight in kg"
+          required
+        />
+        <input
+          type="number"
+          step="0.1"
+          placeholder="Enter you height in cm"
+          required
+        />
+        <button type='submit'>Caluculate</button>
+      </form>
     </Wrapper>
-  )
+  );
 }
 
 export default Calculator;
