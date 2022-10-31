6eff54834eaafafa524930cfcaa320cdaa2fb648
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-31 12:41:05 +0100

    add responsive styles

--- src/app.style.ts
+++ src/app.style.ts
@@ -3,15 +3,25 @@ import styled from "styled-components";
 export const Wrapper = styled.main`
   width: 100vw;
   height: 100vh;
-  background-color: #021e36;
   font-family: Arial, Helvetica, sans-serif;
   color: white;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: baseline;
   align-items: center;
+  margin: 20px auto;
+
+  @media screen and (min-width: 768px){
+    height: 800px;
+    justify-content: center;
+  }
 
   h1 {
-    font-size: 3rem;
+    font-size: 4rem;
+    margin: 15px auto;
+
+    @media screen and (min-width: 768px){
+      font-size: 6rem;
+    }
   }
 `;
