56e4affcbc80f8a64d85a26345136d816cbd4692
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-11-01 07:09:58 +0100

    Update style

--- src/app.style.ts
+++ src/app.style.ts
@@ -3,25 +3,31 @@ import styled from "styled-components";
 export const Wrapper = styled.main`
   width: 100vw;
   height: 100vh;
-  font-family: Arial, Helvetica, sans-serif;
-  color: white;
+  color: #e6e6e6;
   display: flex;
   flex-direction: column;
   justify-content: baseline;
   align-items: center;
   margin: 20px auto;
 
-  @media screen and (min-width: 768px){
+  @media screen and (min-width: 768px) {
     height: 800px;
     justify-content: center;
   }
 
   h1 {
     font-size: 4rem;
+    font-weight: 600;
     margin: 15px auto;
+    text-align: center;
 
-    @media screen and (min-width: 768px){
+    @media screen and (min-width: 768px) {
       font-size: 6rem;
     }
+  }
+
+  span {
+    display: block;
+    color: #79c12c;
   }
 `;
