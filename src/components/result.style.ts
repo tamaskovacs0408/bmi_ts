c4cd824f557aa85ed76d6812fa26f04bdaa05189
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-11-01 07:29:03 +0100

    Update style

---
+++ src/components/result.style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+
+  h2 {
+    font-size: 2.5rem;
+    text-align: center;
+    margin: 10px auto;
+  }
+`;
+
+export const Res = styled.span`
+  margin: 10px auto !important;
+  font-size: 2rem !important;
+`;
