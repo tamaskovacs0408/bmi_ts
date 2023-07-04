1cb6873e5296fe54dbb7690770a07107c74de4ee
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-07-04 19:55:15 +0200

    Change interface file

--- src/components/useCalculateResul.ts
+++
@@ -1,9 +0,0 @@
-export    interface UseCalculateResult {
-  weight: number | string | null;
-  setWeight: React.Dispatch<React.SetStateAction<number | string | null>>;
-  height: number | string | null;
-  setHeight: React.Dispatch<React.SetStateAction<number | string | null>>;
-  bmi: number;
-  weightStatus: string | undefined;
-  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-}
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,13 @@ export interface Props {
   bmi: number | null;
   weightStatus: string | null | undefined;
 }
+
+export interface UseCalculateResult {
+  weight: number | string | null;
+  setWeight: React.Dispatch<React.SetStateAction<number | string | null>>;
+  height: number | string | null;
+  setHeight: React.Dispatch<React.SetStateAction<number | string | null>>;
+  bmi: number;
+  weightStatus: string | undefined;
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
